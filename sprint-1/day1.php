7da45182ab46c5014ef1cb537a98ac28237ad6ef
Inazuma26 <nurhanifahsiddiq@gmail.com>
2023-07-10 11:24:54 +0700

    day1.php

---
+++ sprint-1/day1.php
@@ -0,0 +1,50 @@
+<?php
+// Tipe-tipe Data Pada PHP
+// Tipe Data Number
+// var dump biasa dipakai intuk debuding sama hal nya dengan print_r
+
+var_dump(1234);
+//print_r(1234);
+
+//number menggunakan pemisah berupa garis bawah
+var_dump(1_123_234_456);
+
+var_dump(1.45); //float
+
+// Tipe Data Boolean
+var_dump(true); //benar
+var_dump(false); //salah
+
+//var_dump((bool) "false");
+
+// Tipe Data String -> karakter
+var_dump("karakter");
+echo"\nhanif\tsiddiq";
+
+//Heredoc
+echo <<<Here
+
+inazuma
+        inazuma "hanif"
+
+Here;
+
+// Tipe Data NULL -> tipe data yang tidak punya value
+var_dump (NULL);
+
+//Tipe Data Array -> tipr data yang bisa berisi beberapa jenis tipe data
+var_dump(array(1,true,"false",NULL));
+var_dump([2,false,'true',NULL]);
+
+//VARIABLE -> bisa dirunah
+$nama = "Hanif";
+$umur = 17;
+
+echo "nama saya adalah $nama.\t umur saya adalah $umur";
+
+//CONSTANT / konstanta -> tidak bisa dirubah
+define("NAMA", "HANIF"); // ->menggunakan function bawaan php define
+const UMUR = 17; // ->menggunakan const
+echo "\n";
+echo NAMA . UMUR;
+echo "\n";
