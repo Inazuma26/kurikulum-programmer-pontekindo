f266c8212cd2cad9090a2c0b63a7ea12fc9d6c27
Inazuma26 <nurhanifahsiddiq@gmail.com>
2023-08-04 15:12:38 +0700

    menambahkan semua

---
+++ sprint-4/materi/day13.php
@@ -0,0 +1,43 @@
+<?php
+// database tempat menyimpan data                                                                                       
+// database itu ada 5 macam
+/*
+1.operational database
+-json
+2.database warehouse
+3.distributed database
+basus data yang perangkat penyimpananny tidak satu tempat komputer jadi banyak tmpt penyimpanannya
+4.relational database
+basis data rasuibak adalah basis data yang mengorganisir berdasarkan modal hubungan data
+-MYSQL
+-PostgreSQL
+-MariaDB
+-MongoDB
+-Oracle Database
+-SAP HANA
+-IBM DM2
+-MemSQL
+-Interbase
+-Firebird
+5.End-user Database
+-SQLite
+
+MANFAAT
+kecepatan dan kemudahan
+Multi-user
+kemanan data
+DLL masi bnyk
+
+DBMS (Data Base Management System)
+SQL (Structured Query Language)
+
+perintah mysql jadi 3 bagian
+DDL DML dan DCL
+
+DDL (Data definition language)
+perintah sql yang berhubungan dengan pendefinisian suatu struktur database
+-Create
+DML (data manipulation language)
+merupakan perintah sql yang berhubungan dengan manipulasi atau pengolahan data dalam table
+DCL (Data control language)
+Biasa digunakan untuk merubah hak akses memberikan role dan isu lain */
